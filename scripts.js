f283dd4e75ba9bbf563ac5fb90f64c1a4773e9ab
shivateja98 <164140055+shivateja98@users.noreply.github.com>
2024-04-24 10:14:54 +0530

    Testimonial changes

--- scripts.js
+++ scripts.js
@@ -18,25 +18,3 @@ function toggleNav() {
     hamburger.style.transform = "rotate(0deg)";
   }
 }
-
-const testimonials = document.querySelector('.testimonials');
-const scroller = testimonials.querySelector('.scroller');
-const nextBtn = testimonials.querySelector('.btn.next');
-const prevBtn = testimonials.querySelector('.btn.prev');
-const itemWidth = testimonials.querySelector('.item').clientWidth;
-
-nextBtn.addEventListener('click', scrollToNextItem);
-prevBtn.addEventListener('click', scrollToPrevItem);
-
-function scrollToNextItem() {
-    if(scroller.scrollLeft < (scroller.scrollWidth - itemWidth))
-        scroller.scrollBy({left: itemWidth, top: 0, behavior:'smooth'});
-    else
-        scroller.scrollTo({left: 0, top: 0, behavior:'smooth'});
-}
-function scrollToPrevItem() {
-    if(scroller.scrollLeft != 0)
-        scroller.scrollBy({left: -itemWidth, top: 0, behavior:'smooth'});
-    else
-        scroller.scrollTo({left: scroller.scrollWidth, top: 0, behavior:'smooth'});
-}
